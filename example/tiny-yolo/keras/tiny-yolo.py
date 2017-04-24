c4098d7b7d06be17f671809c782bc88921291b94
natsutani <km1km1km1>
2017-04-24 20:35:58 +0900

    YOLO in working

--- example/tiny-yolo/keras/tiny-yolo.py
+++ example/tiny-yolo/keras/tiny-yolo.py
@@ -3,6 +3,7 @@ from keras.layers import Conv2D, BatchNormalization, MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
 
 from keras import backend as K
+from functools import cmp_to_key
 import numpy as np
 from PIL import Image
 import math
@@ -15,6 +16,7 @@ height = 416
 r_w = 13
 r_h = 13
 r_n = 5
+classes = 20
 
 region_biases = (1.080000, 1.190000, 3.420000, 4.410000, 6.630000, 11.380000, 9.420000, 5.110000, 16.620001, 10.520000)
 
@@ -25,6 +27,13 @@ class box():
         self.y = 0.0
         self.w = 0.0
         self.h = 0.0
+
+
+class sortable_bbox():
+    def __init__(self):
+        self.index = 0
+        self.cls = 0
+        self.probs = None
 
 def tiny_yolo_model():
     model = Sequential()
@@ -74,6 +83,41 @@ def tiny_yolo_model():
     return model
 
 
+def overlap(x1, w1, x2, w2):
+    l1 = x1 - w1/2
+    l2 = x2 - w2/2
+    if l1 > l2:
+        left = l1
+    else:
+        left = l2
+    r1 = x1 + w1/2
+    r2 = x2 + w2/2
+    if r1 < r2:
+        right = r1
+    else:
+        right = r2
+    return right - left
+
+def box_intersection(a, b):
+    w = overlap(a.x, a.w, b.x, b.w)
+    h = overlap(a.y, a.h, b.y, b.h)
+    if w < 0 or h < 0:
+        return 0
+    area = w*h
+    return area
+
+
+def box_union(a, b):
+    i = box_intersection(a, b)
+    u = a.w*a.h + b.w*b.h - i
+    return u
+
+
+def box_iou(a, b):
+    return box_intersection(a, b)/box_union(a, b);
+
+
+1
 def get_region_box(x, biases, n, index, i, j, w, h):
     b = box()
     b.x = (i + logistic_activate(x[index + 0])) / w
@@ -87,19 +131,71 @@ def logistic_activate(x):
     return 1./(1. + math.exp(-x))
 
 def get_region_boxes(predications):
-    classes = 20
 
-    for raw in range(r_h):  # 13
+    for row in range(r_h):  # 13
         for col in range(r_w):  # 13
-            i = raw * r_w + col
+            i = row * r_w + col
             for n in range(r_n):  # 5
-                index = i * r_n + n
+                #index = i * r_n + n
+                index = n
                 # p_index = index * (classes + 5) + 4
                 p_index = classes * n + 4 # 4, 29, 54, 79, 104
-                scale = predications[raw][col][p_index]
+                scale = predications[0][row][col][p_index]
                 box_index = index * (classes * 5)
+                boxes[index] = get_region_box(predications, region_biases[0], n, box_index, col, row, r_w, r_h)
+                boxes[index].x *= r_w
+                boxes[index].y *= r_h
+                boxes[index].w *= r_w
+                boxes[index].h *= r_h
 
-                class_index = index * (classes * 5) + 5
+                #class_index = index * (classes * 5) + 5
+                class_index  = n * (classes + 5)
+
+                for j in range(classes):
+                    prob = scale * predications[0][row][col][class_index + j]
+                    if prob > thresh:
+                        probs[row][col][n][j] = prob
+                    else:
+                        probs[row][col][n][j] = 0
+
+
+def nms_comparator(a, b):
+    ar, ac, an = div_rcn(a.index)
+    br, bc, bn = div_rcn(b.index)
+
+    diff = a.probs[ar][ac][an][b.cls] - b.probs[br][bc][bn][b.cls]
+    if diff < 0:
+        return 1
+    elif diff > 0 :
+        return -1
+    return 0
+
+
+def do_nms_sort(total, classes, thresh):
+    s = [sortable_bbox()] * r_h * r_w * r_n
+    for i, b in enumerate(s):
+        b.index = i
+        b.cls = 0
+        b.probs = probs
+
+    for k in range(classes):
+        for i in range(total):
+            s[i].cls = k
+        s.sort(key=cmp_to_key(nms_comparator))
+
+        for i in range(total):
+            r, c, n  =div_rcn(s[i].index)
+            if probs[r][c][n][k] == 0:
+                continue
+            a = boxes[s[i].index]
+            for j in range(i+1, total):
+                b = boxes[s[j].index]
+                if box_iou(a, b) > thresh:
+                    r2, c2, n2 = div_rcn(s[j].index)
+                    probs[r2][c2][n2][k] = 0
+
+
+
 
 
 
@@ -123,10 +219,15 @@ x = np.expand_dims(image_data, axis=0)
 
 preds = tiny_yolo_model.predict(x)
 boxes = [box()] * r_h * r_w * r_n
-probs = [[[[0.0] * 13] * 13] * 125]
+probs = np.zeros((r_h * r_w * r_n * classes), dtype=np.float)
+thresh = 0.6
 
+print('probs ', probs.shape)
 
+nms = 0.4
 get_region_boxes(preds)
+do_nms_sort(r_w*r_h*r_n, classes, nms)
+
 
 # nms_sort()
 

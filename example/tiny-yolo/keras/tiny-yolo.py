c2cda973cb75c872f97273ecffd1b29c826b33db
natsutani <km1km1km1>
2017-04-27 20:26:48 +0900

    yolo in working

--- example/tiny-yolo/keras/tiny-yolo.py
+++ example/tiny-yolo/keras/tiny-yolo.py
@@ -20,7 +20,8 @@ r_n = 5
 classes = 20
 
 region_biases = (1.080000, 1.190000, 3.420000, 4.410000, 6.630000, 11.380000, 9.420000, 5.110000, 16.620001, 10.520000)
-
+voc_anchors = np.array(
+    [[1.08, 1.19], [3.42, 4.41], [6.63, 11.38], [9.42, 5.11], [16.62, 10.52]])
 
 class box():
     def __init__(self):
@@ -270,6 +271,183 @@ def draw_detections(im, num, thresh, boxes, probs, classes):
                   (cls, prob*100, int(left), int(top), int(right), int(bot)))
 
 
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
+def yolo_head(feats, anchors, num_classes):
+    """Convert final layer features to bounding box parameters.
+
+    Parameters
+    ----------
+    feats : tensor
+        Final convolutional layer features.
+    anchors : array-like
+        Anchor box widths and heights.
+    num_classes : int
+        Number of target classes.
+
+    Returns
+    -------
+    box_xy : tensor
+        x, y box predictions adjusted by spatial location in conv layer.
+    box_wh : tensor
+        w, h box predictions adjusted by anchors and conv spatial resolution.
+    box_conf : tensor
+        Probability estimate for whether each box contains any object.
+    box_class_pred : tensor
+        Probability distribution estimate for each box over class labels.
+    """
+    num_anchors = len(anchors)
+    # Reshape to batch, height, width, num_anchors, box_params.
+    anchors_tensor = np.reshape(anchors, [1, 1, 1, num_anchors, 2])
+
+    # Static implementation for fixed models.
+    # TODO: Remove or add option for static implementation.
+    # _, conv_height, conv_width, _ = K.int_shape(feats)
+    # conv_dims = K.variable([conv_width, conv_height])
+
+    # Dynamic implementation of conv dims for fully convolutional model.
+    conv_dims = feats[0].shape  # assuming channels last
+
+    # In YOLO the height index is the inner most iteration.
+    conv_height_index = np.arange(0, stop=conv_dims[0])
+    conv_width_index = np.arange(0, stop=conv_dims[1])
+    conv_height_index = np.tile(conv_height_index, [conv_dims[1]])
+
+    # TODO: Repeat_elements and tf.split doesn't support dynamic splits.
+    # conv_width_index = K.repeat_elements(conv_width_index, conv_dims[1], axis=0)
+    conv_width_index = np.tile(
+        np.expand_dims(conv_width_index, 0), [conv_dims[0], 1])
+    conv_width_index = np.transpose(conv_width_index).flatten()
+    conv_index = np.transpose(np.stack([conv_height_index, conv_width_index]))
+    conv_index = np.reshape(conv_index, [1, conv_dims[0], conv_dims[1], 1, 2])
+    conv_index = conv_index.astype(np.float)
+
+    feats = np.reshape(
+        feats, [-1, conv_dims[0], conv_dims[1], num_anchors, num_classes + 5])
+
+    # natu atode
+    #conv_dims = np.reshape(conv_dims, [1, 1, 1, 1, 2]).astype(np.float)
+    conv_dims = np.reshape([13.0, 13.0], [1, 1, 1, 1, 2]).astype(np.float)
+
+    # Static generation of conv_index:
+    # conv_index = np.array([_ for _ in np.ndindex(conv_width, conv_height)])
+    # conv_index = conv_index[:, [1, 0]]  # swap columns for YOLO ordering.
+    # conv_index = K.variable(
+    #     conv_index.reshape(1, conv_height, conv_width, 1, 2))
+    # feats = Reshape(
+    #     (conv_dims[0], conv_dims[1], num_anchors, num_classes + 5))(feats)
+
+    box_xy = sigmoid(feats[..., :2])
+    box_wh = np.exp(feats[..., 2:4])
+    box_confidence = sigmoid(feats[..., 4:5])
+    box_class_probs = softmax(feats[..., 5:])
+
+    # Adjust preditions to each spatial grid point and anchor size.
+    # Note: YOLO iterates over height index before width index.
+    box_xy = (box_xy + conv_index) / conv_dims
+    box_wh = box_wh * anchors_tensor / conv_dims
+
+
+    return box_xy, box_wh, box_confidence, box_class_probs
+
+def yolo_boxes_to_corners(box_xy, box_wh):
+    """Convert YOLO box predictions to bounding box corners."""
+    box_mins = box_xy - (box_wh / 2.)
+    box_maxes = box_xy + (box_wh / 2.)
+
+    return np.concatenate([
+        box_mins[..., 1:2],  # y_min
+        box_mins[..., 0:1],  # x_min
+        box_maxes[..., 1:2],  # y_max
+        box_maxes[..., 0:1]  # x_max
+    ])
+
+
+def boolean_mask(xs, masks):
+    ret = []
+    for i in range(xs.shape[0]):
+        xf = xs[i].flatten()
+        mf = masks.flatten()
+        for x, mask in zip(xf, mf):
+            if mask:
+                ret.append(x)
+    return ret
+
+def yolo_filter_boxes(boxes, box_confidence, box_class_probs, threshold=.6):
+    """Filter YOLO boxes based on object and class confidence."""
+
+    threshold = 0.2
+
+    box_scores = box_confidence * box_class_probs
+    box_classes = np.argmax(box_scores, axis=-1)
+    box_class_scores = np.max(box_scores, axis=-1)
+    prediction_mask = box_class_scores >= threshold
+
+    # TODO: Expose tf.boolean_mask to Keras backend?
+    #boxes = boolean_mask(boxes, prediction_mask)
+    #scores = boolean_mask(box_class_scores, prediction_mask)
+    #classes = boolean_mask(box_classes, prediction_mask)
+    dim = boxes.shape
+
+    boxes_f = []
+    scores_f = []
+    classes_f = []
+
+    for r in range(dim[1]):
+        for c in range(dim[2]):
+            for n in range(dim[3]):
+                if prediction_mask[0][r][c][n]:
+                    pos = [boxes[0][r][c][n],boxes[1][r][c][n],boxes[2][r][c][n],boxes[3][r][c][n]]
+
+                    boxes_f.append(pos)
+                    scores_f.append(box_class_scores[0][r][c][n])
+                    classes_f.append(box_classes[0][r][c][n])
+
+    return boxes_f, scores_f, classes_f
+
+
+def yolo_eval(yolo_outputs,
+              image_shape,
+              max_boxes=10,
+              score_threshold=.6,
+              iou_threshold=.5,
+              classes=20):
+    """Evaluate YOLO model on given input batch and return filtered boxes."""
+
+
+    box_xy, box_wh, box_confidence, box_class_probs = yolo_head(yolo_outputs, voc_anchors, classes)
+    boxes = yolo_boxes_to_corners(box_xy, box_wh)
+    boxes, scores, classes = yolo_filter_boxes(
+        boxes, box_confidence, box_class_probs, threshold=score_threshold)
+
+    """
+    # Scale boxes back to original image shape.
+    height = image_shape[0]
+    width = image_shape[1]
+    image_dims = K.stack([height, width, height, width])
+    image_dims = K.reshape(image_dims, [1, 4])
+    boxes = boxes * image_dims
+
+    # TODO: Something must be done about this ugly hack!
+    max_boxes_tensor = K.variable(max_boxes, dtype='int32')
+    K.get_session().run(tf.variables_initializer([max_boxes_tensor]))
+    nms_index = tf.image.non_max_suppression(
+        boxes, scores, max_boxes_tensor, iou_threshold=iou_threshold)
+    boxes = K.gather(boxes, nms_index)
+    scores = K.gather(scores, nms_index)
+    classes = K.gather(classes, nms_index)
+    """
+    return boxes, scores, classes
+
+
 
 
 use_ya2k = False
@@ -285,7 +463,8 @@ else:
     tiny_yolo_model = tiny_yolo_model()
 
 
-tiny_yolo_model.load_weights('weight/tiny-yolo.h5')
+#tiny_yolo_model.load_weights('weight/tiny-yolo.h5')
+tiny_yolo_model.load_weights('weight/tyolo.h5')
 
 with open('tiny-yolo.json', 'w') as fp:
     json_string = tiny_yolo_model.to_json()
@@ -308,6 +487,12 @@ thresh = 0.6
 
 np.save('output/preds%s.npy' % file_post_fix, preds)
 
+out_boxes, out_scores, out_classes = yolo_eval(preds, (width, height), thresh, 0.5, classes = classes)
+
+
+
+
+sys.exit(1)
 debug_layer = 28
 #layer_dump(tiny_yolo_model, x, debug_layer, file_post_fix)
 
